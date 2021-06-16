a736072036b2bb928b48e06a7d12b3ffbf8387ee
raida <raida@student.42.jp>
2021-06-16 21:56:18 +0900

    ex00の作成完了しました 添削お願いします

---
+++ ex00/main.py
@@ -0,0 +1,15 @@
+import product as pd
+import salesperson as sp
+
+
+def main():
+	p = pd.Product()
+	print(p)
+
+	s = sp.Salesperson(name="salesman")
+	print(s)
+
+	print(s.promote())
+
+if __name__ == '__main__':
+	main()
---
+++ ex01/main.py
@@ -0,0 +1,17 @@
+import product as pd
+import beverage as bv
+
+
+def main():
+    p = pd.Product(name="apple", price=100, description="apple fruits")
+    print(p.print_attr())
+
+    print("+++++++++++++++++++")
+
+    b = bv.Beverage(
+        name="apple", price=100, description="apple fruits", temperature=100
+    )
+    print(b.print_attr())
+
+if __name__ == "__main__":
+    main()
