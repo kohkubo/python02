849999bb986c59490074a36a10e91973ff385866
raida <raida@student.42.jp>
2021-06-17 14:57:32 +0900

    ex02途中段階

---
+++ ex02/main.py
@@ -0,0 +1,18 @@
+import beverage as bv
+import product as pd
+import vendingmachine as vm
+
+
+def main():
+
+	p = pd.Product(name="coffee", price=100, description="beverage")
+	print(p.print_attr())
+	print("++++++++++++++++++++++++++++")
+	b = bv.Beverage(name="coffee", price=100, description="beverage", temperature=5)
+	print(b.print_attr())
+	print("++++++++++++++++++++++++++++")
+	v = vm.Vendingmachine(name="coffee", stock=10)
+	v.ask()
+
+if __name__ == '__main__':
+	main()
---
+++ ex02/vendingmachine.py
@@ -0,0 +1,39 @@
+import beverage as b
+
+
+class Vendingmachine:
+	def __init__(self, name, stock):
+		"""
+		taking a string, an array of Beverage instances as parameters,
+		assign its value to a name attribute and a stock attribute.
+		"""
+		self.name = name
+		self.stock = stock
+
+	def __str__(self):
+		"""
+		return its name attributes in the format.
+		<name attribute> the vending machine
+		"""
+		return "{0}\n".format(self.name)
+
+	def sell(self, beverage_name):
+		"""
+		taking a string as a parameter, print a message of your choice.
+		the message differ when there is a Beverage instance
+		named same as product_name in the stock, or not.
+		"""
+		self.beverage_name = beverage_name
+		if (self.beverage_name == self.name and self.stock != 0):
+			print("Here is your " + self.beverage_name + "!")
+		else:
+			print("Sorry! I do not have " + self.beverage_name + "...")
+
+	def ask(self):
+		"""
+		print a message to ask the beverage name,
+		store the user input,
+		then call sell method with the users input as parameter.
+		"""
+		bname = input("Hello, what would you like?\n")
+		self.sell(beverage_name=bname)
