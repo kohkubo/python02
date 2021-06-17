0aa9607142ade19b9d7213bc02b4e355bc988707
raida <raida@student.42.jp>
2021-06-17 15:44:16 +0900

    tab->spaceに修正

--- ex02/main.py
+++ ex02/main.py
@@ -5,14 +5,14 @@ import vendingmachine as vm
 
 def main():
 
-	p = pd.Product(name="coffee", price=100, description="beverage")
-	print(p.print_attr())
-	print("++++++++++++++++++++++++++++")
-	b = bv.Beverage(name="coffee", price=100, description="beverage", temperature=5)
-	print(b.print_attr())
-	print("++++++++++++++++++++++++++++")
-	v = vm.Vendingmachine(name="coffee", stock=10)
-	v.ask()
+    p = pd.Product(name="coffee", price=100, description="beverage")
+    print(p.print_attr())
+    print("++++++++++++++++++++++++++++")
+    b = bv.Beverage(name="coffee", price=100, description="beverage", temperature=5)
+    print(b.print_attr())
+    print("++++++++++++++++++++++++++++")
+    v = vm.Vendingmachine(name="coffee", stock=10)
+    v.ask()
 
 if __name__ == '__main__':
-	main()
+    main()
--- ex02/product.py
+++ ex02/product.py
@@ -1,26 +1,26 @@
 class Product:
-	def __init__(self, name, price, description):
-		"""
-		assign its values to a name attribute and a price attribute.
-		"""
-		self.name = name
-		self.price = price
-		self.description = description
+    def __init__(self, name, price, description):
+        """
+        assign its values to a name attribute and a price attribute.
+        """
+        self.name = name
+        self.price = price
+        self.description = description
 
-	def __str__(self):
-		"""
-		return its name and description attributes in the format.
-		<name attribute> : <description attribute>
-		"""
-		return "{0} : {1}".format(self.name, self.description)
+    def __str__(self):
+        """
+        return its name and description attributes in the format.
+        <name attribute> : <description attribute>
+        """
+        return "{0} : {1}".format(self.name, self.description)
 
-	def print_attr(self):
-		"""
-		return its name, price and description attributes in the format.
-		name : <name attribute>
-		price : <price attribute limited to two decimal points>
-		description : <description attribute>
-		"""
-		return "name : {0}\nprice : {1}\ndescription : {2}".format(
-			self.name, self.price, self.description
-		)
+    def print_attr(self):
+        """
+        return its name, price and description attributes in the format.
+        name : <name attribute>
+        price : <price attribute limited to two decimal points>
+        description : <description attribute>
+        """
+        return "name : {0}\nprice : {1}\ndescription : {2}".format(
+            self.name, self.price, self.description
+        )
